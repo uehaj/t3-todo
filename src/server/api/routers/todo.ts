2c010482fd718669bc852c20397b812799b0dda8
Junji UEHARA <uehera.junji@ntt-tx.co.jp>
2023-04-10 01:01:02 +0900

    rename t3-todo

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -1,9 +1,36 @@
 import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const todoRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.todo.findMany();
   }),
+  add: publicProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      const { text } = input;
+      return ctx.prisma.todo.create({
+        data: {
+          done: false,
+          text,
+        },
+      });
+    }),
+  delete: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(({ ctx, input }) => {
+      const { id } = input;
+      return ctx.prisma.todo.delete({ where: { id } });
+    }),
+  done: publicProcedure
+    .input(z.object({ id: z.string(), done: z.boolean() }))
+    .mutation(({ ctx, input }) => {
+      const { id, done } = input;
+      console.log(`id=`, id, ` done=`, done);
+      return ctx.prisma.todo.update({ where: { id }, data: { done } });
+    }),
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,4 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
 } satisfies Config;
