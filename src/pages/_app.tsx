71d3201af7f40026fb0b4c0543527a12c699578c
@uehaj <59012+uehaj@users.noreply.github.com>
2023-05-07 19:03:31 +0900

    add tanstack query DevTool

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,23 @@
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useEffect } from "react";
+import "~/styles/globals.css";
+import { QueryClientProvider, useQueryClient } from "@tanstack/react-query";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   useEffect(() => {
     const body = document.body;
     body.setAttribute("data-theme", "garden");
   }, []);
-  return <Component {...pageProps} />;
+  const queryClient = useQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -3,7 +3,13 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const todoRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.todo.findMany();
+    return ctx.prisma.todo.findMany({
+      orderBy: [
+        {
+          id: "desc",
+        },
+      ],
+    });
   }),
   add: publicProcedure
     .input(
