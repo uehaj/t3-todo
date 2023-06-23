1aea34b77a53187ec2c0d1f6ffc699f072b40627
@uehaj <59012+uehaj@users.noreply.github.com>
2023-06-23 14:54:24 +0900

    inferetor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,25 +5,36 @@ import "~/styles/globals.css";
 import { Refine } from "@refinedev/core";
 import dataProvider from "@refinedev/simple-rest";
 import routerProvider from "@refinedev/nextjs-router";
+import { ThemedLayoutV2, RefineThemes } from "@refinedev/mantine";
+import { MantineProvider, Global } from "@mantine/core";
+import { MantineInferencer } from "@refinedev/inferencer/mantine";
 
 const App: AppType = ({ Component, pageProps }) => {
   return (
-    <Refine
-      options={{ disableTelemetry: true }}
-      dataProvider={dataProvider("http://localhost:3000/api")}
-      routerProvider={routerProvider}
-      resources={[
-        {
-          name: "todo",
-          list: "/todo",
-          show: "/todo/show/:id",
-        },
-      ]}
+    <MantineProvider
+      theme={{ colorScheme: "dark" }}
+      withNormalizeCSS
+      withGlobalStyles
     >
-      {/*      <Layout>*/}
-      <Component {...pageProps} />
-      {/*      </Layout>*/}
-    </Refine>
+      <Global styles={{ body: { WebkitFontSmoothing: "auto" } }} />
+      <Refine
+        options={{ disableTelemetry: true }}
+        dataProvider={dataProvider("http://localhost:3000/api")}
+        routerProvider={routerProvider}
+        resources={[
+          {
+            name: "todo",
+            list: "/todo",
+            show: "/todo/show/:id",
+          },
+        ]}
+      >
+        <ThemedLayoutV2>
+          {/*<Component {...pageProps} />*/}
+          <MantineInferencer />
+        </ThemedLayoutV2>
+      </Refine>
+    </MantineProvider>
   );
 };
 
